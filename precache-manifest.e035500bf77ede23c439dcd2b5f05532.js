99767f80b02ebf352a8e0582c75407b5e32b7f59
shbt <baekgor7@naver.com>
2020-07-23 16:30:47 +0900

    Updates

---
+++ precache-manifest.e035500bf77ede23c439dcd2b5f05532.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f97b2daed4dc7f9da5eaec85c056ea8",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "f606bf63db779c933bb7",
+    "url": "/movie_app_2020/static/css/main.a433c436.chunk.css"
+  },
+  {
+    "revision": "5b635e2e420b910582ea",
+    "url": "/movie_app_2020/static/js/2.6a4a412e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.6a4a412e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f606bf63db779c933bb7",
+    "url": "/movie_app_2020/static/js/main.9fa31767.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
